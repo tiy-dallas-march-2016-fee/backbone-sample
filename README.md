11a5f0850486ed5b9df78ac342d2db3ed6d40da0
Eric Sowell <eric.sowell@gmail.com>
2016-05-20 09:16:28 -0500

    implemented gulp-sass

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ This is an annotated backbone sample for your reference. In includes all the set
 ## Gulp
 
 To setup Babel with gulp, you will want to use `gulp-babel`. [Follow the instructions here](https://www.npmjs.com/package/gulp-babel) to set it up.
+
+### Babel
+
+For Babel with Gulp, use [gulp-babel](https://www.npmjs.com/package/gulp-babel).
+
+### Sass
+
+For Sass with Gulp, use [gulp-sass](https://www.npmjs.com/package/gulp-sass).
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp');
-var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 
-gulp.task('default', ['js-build', 'watch']);
+gulp.task('default', ['js-build', 'sass-build', 'watch']);
 
 
 gulp.task('watch', () => {
   gulp.watch('src/js/**/*.js', ['js-build']);
+  gulp.watch('src/scss/**/*.scss', ['sass-build']);
 });
 
 
@@ -21,4 +22,12 @@ gulp.task('js-build', () => {
 		.pipe(concat('all.js'))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('public/js'));
+});
+
+gulp.task('sass-build', function () {
+ return gulp.src('./src/scss/style.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./public/css/style.css'));
 });
